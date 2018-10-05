b8efd713082c18647275e02a9e1149259d209b89
Nikolaos Papadakis <nickpapadakhs@gmail.com>
2018-10-05 16:14:14 +0300

    Initial Commit

---
+++ Source/HHH/HHHGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HHHGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HHH_API AHHHGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
